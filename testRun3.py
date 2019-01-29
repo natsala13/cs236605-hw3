f5cdb5446d1fd443ecfae45d20aa7f4027f1d66e
natsala13 <natouda@Nathans-MacBook-Pro.local>
2019-01-29 18:21:53 +0200

    run 3.8

--- testRun3.py
+++ testRun3.py
@@ -6,19 +6,19 @@ from hw3.autoencoder import AutoEncoderError
 import numpy as np
      
 Init_Name = 'MEGA_RUN'
-OUTDIR = './results/Res7/'
+OUTDIR = './results/Res8/'
 
 AllResults = {}
 
 bs = 16
 Epochs = 40
-s = 0.95
+# s = 0.95
 lr = 0.0005
 
 
 def oneExp(h,z,s):
     try:
-        name = 'h_' + str(h) + 'z_' + str(z) + 'lr_' + str(lr)
+        name = 'h_' + str(h) + 'z_' + str(z) + 's_' + str(s)
         res = run_experiment(Init_Name + name, out_dir=OUTDIR, seed=42,
                              # Training params
                              bs_train=bs, bs_test=None, batches=100, epochs=Epochs,
@@ -35,6 +35,7 @@ def oneExp(h,z,s):
 
 for h in [128,256]:
     for z in [20,50]:
+        for s in [0.85,0.9,0.95]
             oneExp(h,z,s)
             
 

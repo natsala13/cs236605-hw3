84f23aa61f4cd8b7d4330d79939a92c9688606ee
natsala13 <natouda@Nathans-MacBook-Pro.local>
2019-01-31 19:52:09 +0200

    syntax

--- hw3/autoencoder.py
+++ hw3/autoencoder.py
@@ -78,7 +78,7 @@ class DecoderCNN(nn.Module):
     def forward(self, h):
         # Tanh to scale to [-1, 1] (same dynamic range as original images).
         x = self.tanh.cnn(h)
-	return torch.tanh(x)
+        return torch.tanh(x)
 
 
 class VAE(nn.Module):

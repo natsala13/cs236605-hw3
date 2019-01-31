af49102aa7e0c0c0cd0af2e9f061d64cdeaf519d
natsala13 <natouda@Nathans-MacBook-Pro.local>
2019-01-31 19:10:36 +0200

    Create runGan.py

---
+++ runGan.py
@@ -0,0 +1,38 @@
+# Lets try an experiment
+
+from hw3.answers import part2_vae_hyperparams
+from hw3.experiments import run_experiment
+from hw3.autoencoder import AutoEncoderError 
+import numpy as np
+     
+Init_Name = 'MEGA_RUN'
+OUTDIR = './results/Res1/'
+
+AllResults = {}
+
+bs = 16
+h_dim = 256
+z_dim = 128
+Epochs = 100
+lr = 0.0005
+
+
+
+
+try:           
+    res = run_experiment_GAN('Gan_First_Run', out_dir='results/', seed=42,
+                            # Training params
+                            bs_train=8, bs_test=None, batches=100, epochs=100,
+                            early_stopping=10, checkpoints=None,
+                            # Model params
+                            h_dim=256, z_dim=128)
+    AllResults[name] = res
+except AutoEncoderError as e:
+    AllResults[name] = 'Failed... ' + str(e)
+except OSError as e:
+    AllResults[name] = 'Failed... ' + str(e)
+    
+    
+    
+            
+np.save(OUTDIR + 'RunFinal', AllResults)
